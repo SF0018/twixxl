53df48bcf57c3db19021214ec192ba0df3d42b94
SF180 <sheldon08ferns@gmail.com>
2022-01-22 20:33:23 +0300

    added the 1st mockup

---
+++ action_page.php
@@ -0,0 +1,34 @@
+?<php>
+
+
+</php>
+
+
+<!DOCTYPE html>
+    <!-- Meta Tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="">
+	<meta name="msapplication-TileColor" content="#ff5cad">
+	<meta name="theme-color" content="#ff5cad">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="">
+	<meta name="description" content="Twwixl a website where you can connect with your friends and family.">
+	<meta name="keywords" content="twwixl">
+	<meta property="og:title" content="" />
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="Twwixl a website where you can connect with your friends and family." />
+	<meta property="og:site_name" content="twwixl.com">  
+	<meta itemprop="name" content="">  
+  <meta property="og:image" content="img/twwixl.svg"/>
+	<meta itemprop="description" content="">  
+    <meta name="twitter:title" content="Twwixl">  
+    <meta name="twitter:description" content="Twwixl a website where you can connect with your friends and family.">  
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no" />
+    <meta charset="web-link" />
+	<link rel="icon" href="img/twwixl.svg">
+	<link rel="shortcut icon" id="favicon" href="img/twwixl.svg">
+	<link rel="icon" type="image/png" href="img/twwixl.svg">
+	<link rel="icon" href="img/twwixl.svg">
+<head>
+	<meta charset="utf-8">
+	<title>Signup Page | twwixl</title>
