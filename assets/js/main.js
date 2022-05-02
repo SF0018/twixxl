9b5ba63067a0c487b261533f770d83d4844504fb
SF180 <sheldon08ferns@gmail.com>
2022-05-02 20:49:25 +0300

    dark mode (beta)

--- assets/js/main.js
+++ assets/js/main.js
@@ -246,5 +246,26 @@ function updateOsDownloadButton(osDetails) {
  /**
    * Counter
    */
+if (updateOsDownloadButton.target);
 
+
+
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
   
